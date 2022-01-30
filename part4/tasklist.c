a3f3e661b62882abebdfe47c268e7da2752c1115
Paparazziii <finiantang0826@gmail.com>
2022-01-30 05:12:50 -0500

    first draft for part4

--- part4/tasklist.c
+++ part4/tasklist.c
@@ -1,8 +1,45 @@
 #include <linux/module.h>
+#include <linux/sched/task.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+#include <linux/printk.h>
+
+
+struct task_struct *root = &init_task;
+//int cur_level0 = 0;
+
+void printTask(struct task_struct *task, int cur_level){
+	int num_spaces = 4*min(cur_level,20);
+	char ran[] = "abcd";
+	//printk(KERN_INFO "%*c %s", num_spaces, ' ', ran);
+	//printk(KERN_INFO "\_ ")
+	printk(KERN_INFO "%*c\\_ [%d] %s \n", num_spaces, ' ', task->pid, task->comm);
+}
+
+void traverseAll(struct task_struct *task, int cur_level){
+	struct list_head *child;
+	struct list_head *list;
+	struct task_struct *curr;
+	child = &task->children;
+	if(child==NULL){
+	    cur_level = 0;
+	    return;
+	}
+	cur_level = cur_level+1;
+	list_for_each(list, child){
+	    curr = list_entry(list, struct task_struct, sibling);
+	    printTask(curr, cur_level);
+	    //cur_level = cur_level+1;
+	    traverseAll(curr, cur_level);
+	}
+}
 
 int tasklist_init(void)
 {
 	printk(KERN_INFO "Loading Module\n");
+
+	printk(KERN_INFO "-- [%d] %s \n", root->pid, root->comm); 
+	traverseAll(root,0);
 
 	return 0;
 }
